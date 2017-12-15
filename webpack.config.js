65562b3389d02201168412996cfb1b3728b31c0b
Fakhruddin <fakhruddin.shaukat@hotmail.com>
2017-12-15 20:08:11 +0400

    html + clean plugins for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: path.resolve(__dirname,'src','index.ts'),
@@ -14,6 +16,10 @@ module.exports = {
     resolve: {
         extensions : ['.ts','.tsx','.js']
     },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({title: 'Polychain'})
+    ],
     output:{
         filename: 'bundle.js',
         path: path.resolve(__dirname,'dist')
